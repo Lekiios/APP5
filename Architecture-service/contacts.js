5bacdd40b83efbdcfc9e0df23cd4ea43640eb603
Lekiios <lorenzo.lacz@gmail.com>
2025-02-04 23:41:22 +0100

    NodeJS - TP3 - OK

--- Architecture-service/contacts.js
+++ Architecture-service/contacts.js
@@ -1,36 +1,61 @@
 const chalk = require("chalk");
-const fs = require("fs")
-const yargs = require('yargs/yargs')
-const {hideBin} = require('yargs/helpers')
-const _ = require('lodash');
+const fs = require("fs");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const _ = require("lodash");
 
+/**
+ * Colorize a string with Chalk
+ * @param str string to colorize
+ * @param color color to use (see Chalk documentation)
+ * @returns {*} colorized string for console output
+ */
 function colored(str, color) {
-    return args.colors ? chalk[color](str) : str;
+  return args.colors ? chalk[color](str) : str;
 }
 
+/**
+ * Check if a string is a valid JSON
+ * @param str string to check
+ * @returns {boolean} true if string is a valid JSON, false otherwise
+ */
 function isValidJSON(str) {
-    try {
-        JSON.parse(str);
-        return true;
-    } catch (e) {
-        if (args.debug) console.error(e);
-        return false;
-    }
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    if (args.debug) console.error(e);
+    return false;
+  }
 }
 
-
+/**
+ * Represents a contact with a first name, a last name, an address and a phone number
+ */
 class Contact {
-    constructor(id, firstName, lastName, address, phone) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
-    }
+  /**
+   * Create a new contact
+   * @param id id of the contact
+   * @param firstName first name of the contact
+   * @param lastName last name of the contact
+   * @param address address of the contact
+   * @param phone phone number of the contact
+   */
+  constructor(id, firstName, lastName, address, phone) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phone = phone;
+  }
 
-    toString() {
-        return `${colored(this.lastName.toUpperCase(), "blue")} ${colored(this.firstName, "red")}`;
-    }
+  /**
+   * Get a string representation of the contact (full name)
+   * @returns {string} full name of the contact
+   */
+  toString() {
+    return `${colored(this.lastName.toUpperCase(), "blue")} ${colored(this.firstName, "red")}`;
+  }
 }
 
 /*class ContactService {
@@ -48,111 +73,177 @@ class Contact {
     }
 }*/
 
+/**
+ * Service to manage contacts in a file
+ */
 class FileContactService {
-    path = 'contacts.json';
+  path = "contacts.json";
 
-    read(cb) {
-        fs.readFile(this.path, 'utf-8', (err, data) => {
-            if (err) {
-                if (args.debug) console.error(err);
-                return;
-            }
-            if (!isValidJSON(data)) {
-                return;
-            }
-            cb(JSON.parse(data).map((c) => new Contact(c.id, c.firstName, c.lastName, c.address, c.phone)));
-        });
-    }
+  /**
+   * Read contacts from file
+   * @param cb callback function called with contacts list
+   */
+  read(cb) {
+    fs.readFile(this.path, "utf-8", (err, data) => {
+      if (err) {
+        if (args.debug) console.error(err);
+        return;
+      }
+      if (!isValidJSON(data)) {
+        return;
+      }
+      cb(
+        JSON.parse(data).map(
+          (c) => new Contact(c.id, c.firstName, c.lastName, c.address, c.phone),
+        ),
+      );
+    });
+  }
 
-    write(contacts, cb) {
-        fs.writeFile(this.path, JSON.stringify(contacts), (err) => {
-            if (err) {
-                if (args.debug) console.error(err);
-                return;
-            }
-            cb(contacts);
-        });
-    }
+  /**
+   * Write contacts to file
+   * @param contacts contacts list to write
+   * @param cb callback function called with contacts list
+   */
+  write(contacts, cb) {
+    fs.writeFile(this.path, JSON.stringify(contacts), (err) => {
+      if (err) {
+        if (args.debug) console.error(err);
+        return;
+      }
+      cb(contacts);
+    });
+  }
 
-    get contacts() {
-        return this.read((c) => c);
-    }
+  /**
+   * Get contacts list
+   */
+  get contacts() {
+    return this.read((c) => c);
+  }
 
-    print() {
-        console.log(chalk.underline("Contact list :"));
-        this.read(c => c.forEach((c) => console.log(c.toString())))
-    }
+  /**
+   * Print contacts list
+   */
+  print() {
+    console.log(chalk.underline("Contact list:"));
+    this.read((c) => c.forEach((c) => console.log(c.toString())));
+  }
 
-    add(firstName, lastName, cb) {
-        this.read((contacts) => {
-            const contact = new Contact(contacts[contacts.length - 1].id + 1, firstName, lastName, "", "");
-            contacts.push(contact);
-            this.write(contacts, cb);
-        });
-    }
+  /**
+   * Add a contact to the list
+   * @param firstName first name of contact
+   * @param lastName last name of contact
+   * @param cb callback function called with contacts list
+   */
+  add(firstName, lastName, cb) {
+    this.read((contacts) => {
+      const contact = new Contact(
+        contacts[contacts.length - 1].id + 1,
+        firstName,
+        lastName,
+        "",
+        "",
+      );
+      contacts.push(contact);
+      this.write(contacts, cb);
+    });
+  }
 
-    delete(id, cb) {
-        this.read((contacts) => {
-            const index = contacts.findIndex((c) => c.id === id);
-            if (index !== -1) {
-                const contact = contacts.splice(index, 1)[0];
-                this.write(contacts, cb);
-            }
-        });
-    }
+  /**
+   * Delete a contact from the list
+   * @param id id of contact to delete
+   * @param cb callback function called with contacts list
+   */
+  delete(id, cb) {
+    this.read((contacts) => {
+      const index = contacts.findIndex((c) => c.id === id);
+      if (index !== -1) {
+        this.write(contacts.splice(index, 1)[0], cb);
+      }
+    });
+  }
 
-    watch(cb) {
-        fs.watch(this.path, (eventType, filename) => {
-            this.read((contacts) => {
-                cb(eventType, filename);
-            });
-        });
-    }
+  /**
+   * Watch the contacts file for changes
+   * @param cb callback function called with evenType, previous and new contacts list
+   */
+  watch(cb) {
+    let content;
+    this.read((contacts) => (content = contacts));
+
+    fs.watch(this.path, (eventType) => {
+      this.read((contacts) => {
+        cb(eventType, content, contacts);
+      });
+    });
+  }
 }
 
 const service = new FileContactService();
 
 const args = yargs(hideBin(process.argv))
-    .command('list', 'List contacts', () => null, () => {
+  .command(
+    "list",
+    "List contacts",
+    () => null,
+    () => {
+      service.print();
+    },
+  )
+  .command(
+    "add <firstName> <lastName>",
+    "Add a contact",
+    (yargs) => {
+      yargs
+        .positional("firstName", {
+          type: "string",
+          description: "First name of contact to add",
+        })
+        .positional("lastName", {
+          type: "string",
+          description: "Last name of contact to add",
+        });
+    },
+    (argv) => {
+      service.add(argv.firstName, argv.lastName, () => {
+        console.log("Contact has been added");
         service.print();
-    }).command('add <firstName> <lastName>', 'Add a contact', (yargs) => {
-        yargs.positional('firstName', {
-            type: 'string',
-            description: 'First name of contact to add'
-        }).positional('lastName', {
-            type: 'string',
-            description: 'Last name of contact to add'
-        });
-    }, (argv) => {
-        service.add(argv.firstName, argv.lastName, () => {
-            console.log("Contact has been added");
-            service.print();
-        });
-    }).command('delete <id>', 'Delete a contact', (yargs) => {
-        yargs.positional('id', {
-            type: 'number',
-            description: 'Id of contact to delete'
-        });
-    }, (argv) => {
-        service.delete(argv.id, () => {
-            console.log("Contact has been deleted");
-            service.print();
-        });
-    }).command('watch', 'Watch contacts file', {}, () => {
-        service.watch((eventType, filename) => {
-            console.log(`File ${filename} has been ${eventType}`)
-        });
-    })
-    .option('colors', {
-        alias: 'C',
-        type: 'boolean',
-        description: 'Activate colors in the output'
-    })
-    .option('debug', {
-        alias: 'D',
-        type: 'boolean',
-        description: 'Activate debug mode'
-    })
-    .parse()
-
-
+      });
+    },
+  )
+  .command(
+    "delete <id>",
+    "Delete a contact",
+    (yargs) => {
+      yargs.positional("id", {
+        type: "number",
+        description: "Id of contact to delete",
+      });
+    },
+    (argv) => {
+      service.delete(argv.id, () => {
+        console.log("Contact has been deleted");
+        service.print();
+      });
+    },
+  )
+  .command("watch", "Watch contacts file", {}, () => {
+    service.watch((eventType, prev, next) => {
+      if (eventType === "change") {
+        console.log(chalk.underline("Contacts have changed:"));
+        console.log(_.differenceWith(prev, next, _.isEqual));
+      }
+    });
+  })
+  .option("colors", {
+    alias: "C",
+    type: "boolean",
+    description: "Activate colors in the output",
+  })
+  .option("debug", {
+    alias: "D",
+    type: "boolean",
+    description: "Activate debug mode",
+  })
+  .parse();
